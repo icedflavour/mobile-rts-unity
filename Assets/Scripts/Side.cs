c17c0ba7c4b58318fc823ddcea8834e56f0c861c
Valentin <iced.flavour@gmail.com>
2024-09-02 21:58:21 +0300

    Added colors for 2 players

--- Assets/Scripts/Side.cs
+++ Assets/Scripts/Side.cs
@@ -6,8 +6,27 @@ public class Side : MonoBehaviour
 
     public GameObject MyPlayer;
 
+    private MeshRenderer myRenderer;
+
+    private Color Blue = new Color(35f / 255f, 80f / 255f, 200f / 255f);
+    private Color Red = new Color(200f / 255f, 35f / 255f, 35f / 255f);
+    
     private void Awake()
     {
+        myRenderer = GetComponent<MeshRenderer>();
+
+        switch (mySide)
+        {
+            case PlayingSide.PlayerOne:
+                myRenderer.material.SetColor("_Color", Blue);
+                break;
+            case PlayingSide.PlayerTwo:
+                myRenderer.material.SetColor("_Color", Red);
+                break;
+            default:
+                break;
+        }
+
         var Players = FindObjectsOfType<Player>();
 
         foreach (Player playerEntity in Players)
